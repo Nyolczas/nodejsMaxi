00a7984151ce62f33ab984ecc8cfca334084fcfa
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2019-01-30 15:40:37 +0100

    Update first-app.js

--- first-app.js
+++ first-app.js
@@ -1,3 +1,18 @@
-const fs = require('fs');
+var name = 'Isti';
+var age = 44;
+var hasHobbies = true;
 
-fs.writeFileSync('hello.txt', 'Hello from Node.js');
+function summarizeUser(userName, userAge, userHasHobby) {
+    let boolConversion;
+    if(userHasHobby){
+        boolConversion='van';
+    }
+    else{
+        boolConversion='nincs';
+    }
+    return (
+        'A nevem ' + userName + ', ' + userAge + ' éves vagyok és ' + boolConversion + ' hobbim.'
+    );
+} 
+
+console.log(summarizeUser(name, age, hasHobbies));
