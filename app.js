c0ef1e4a0d5502902067848d3287e6950b87922b
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2019-01-31 14:19:17 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    //process.exit();
+    // ezt kapja
+    //console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        // ha az url az alap, akkor ezt adja vissza alapból
+        res.write('<html lang="en">');
+        res.write(`<head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" 
+                        content="width=device-width, initial-scale=1.0">
+                        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                        <title>Enter Message</title>
+                    </head>`);
+        res.write(`<body>
+                        <form action="/message" method="POST">
+                            <input type="text" name="message">
+                            <button type="submit">Küldés</button>
+                        </form>
+                    </body>`);
+        res.write('</html>');
+        return res.end(); // innen nem megy tovább
+    }
+
+    // az üzenet beírása után ezt adja vissza
+    if(url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (adatDarab) => { //gyűjti a bejövő adatot
+            //console.log(adatDarab);
+            body.push(adatDarab);
+        });
+        return req.on('end', () => { // összeállítja a bejövő adatot.
+            const parsedBody = Buffer.concat(body).toString();
+            //console.log(parsedBody);
+            const message = parsedBody.split('=')[1]; // a kulcs-érték párból kinyeri az értéket
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/'); // visszairányítja az induló oldalra.
+                return res.end(); // innen nem megy tovább
+            });
+        });
+    }
+
+    // ezt adja vissza minden más esetben
     res.setHeader('Content-Type', 'text/html');
     res.write('<html lang="en">');
     res.write(`<head>
