0d756acef7692feedde90121d8cd2123fdcf423f
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2019-02-01 17:59:06 +0100

    app02 server
    Express.js

--- app02/server.js
+++ app02/server.js
@@ -1,3 +1,16 @@
-const http = require('http');
-const server = http.createServer();
-server.listen(3000);
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('Benne vagyunk a Middleware-ben!');
+    next(); // A next engdélyezi a továbbjutást a következő Middleware-be.
+});
+
+app.use((req, res, next) => {
+    console.log('Benne vagyunk egy másik Middleware-ben!');
+    //res.setHeader(''); // ezzel felülírhazjuk az alapértelmezett headert, amit az express ad
+    res.send('<h1>Hello az Express.js-ből!!!</h1>'); // az express automatikusan belerakja egy html vázba.
+});
+
+app.listen(3000);
