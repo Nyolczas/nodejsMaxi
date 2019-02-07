826dbbac65070dd849867a1790d09d98b9e3ff94
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2019-02-07 22:03:54 +0100

    Update server.js

--- app02/server.js
+++ app02/server.js
@@ -2,12 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Benne vagyunk a Middleware-ben!');
-    next(); // A next engdélyezi a továbbjutást a következő Middleware-be.
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1>Ez az "Add-product" oldal.</h1>'); // az express automatikusan belerakja egy html vázba.
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('Benne vagyunk egy másik Middleware-ben!');
     //res.setHeader(''); // ezzel felülírhazjuk az alapértelmezett headert, amit az express ad
     res.send('<h1>Hello az Express.js-ből!!!</h1>'); // az express automatikusan belerakja egy html vázba.
