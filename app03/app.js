1f012717967ded67a44c19714421acc6a071dac1
Nyolczas <nyolczas@gmail.com>
2019-02-09 10:02:39 +0100

    app03 update
    Routing

--- app03/app.js
+++ app03/app.js
@@ -3,22 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send(`<form action="/product" method="POST">
-                <input type="text" name="bejovo-adat">
-                <button type="submit">Küldés</button>
-            </form>`); 
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello az Express.js-ből!!!</h1>'); 
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
